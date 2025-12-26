7bd6b95ad6b91c64a93a09558da34ef370b7e7f7
JoeDev37 <alonejoe367@gmail.com>
2025-12-26 20:16:28 +0300

    Age Calculator(coding)

--- main.js
+++ main.js
@@ -16,7 +16,27 @@ btn.addEventListener('click', () => {
     const value = date.value;
 
     if(!value) {
-        console.log('Selecte A Date');
+        const popupThing = document.createElement('div');
+        popupThing.textContent = 'Select first';
+        popupThing.style.backgroundColor = '#f38ba8';
+        popupThing.style.color = '#11111b';
+        popupThing.style.width = '100%';
+        popupThing.style.height = '20px';
+        popupThing.style.textAlign = 'center';
+        popupThing.style.borderRadius = '7px';
+
+        popupThing.style.position = 'fixed';
+        popupThing.style.top = '20px';
+        popupThing.style.left = '50%';
+        popupThing.style.transform = 'translateX(-50%)';
+        popupThing.style.zIndex = '9999';
+
+        document.body.appendChild(popupThing)
+
+        setTimeout(() => {
+            popupThing.remove();
+        }, 2000);
+
         return;
     };
 
@@ -34,9 +54,6 @@ btn.addEventListener('click', () => {
     const ageD = today.getDate() - day;
     const ageM = today.getMonth() + 1 - month;
     const ageY = today.getFullYear() - year;
-
-
-
 
     days.textContent = ageD;
     months.textContent = ageM;
